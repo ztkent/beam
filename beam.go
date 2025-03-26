de492a8e36673def7fa66d0a98033e356273c2fe
ztkent <ztkent@gmail.com>
2025-03-25 20:00:59 -0400

    create a general 2D map structure

--- beam.go
+++ beam.go
@@ -13,3 +13,42 @@ const (
 	StateSettings
 	StateHighScores
 )
+
+type Position struct {
+	X, Y int
+}
+
+type TileType int
+
+func (tile TileType) ToInt() int {
+	return int(tile)
+}
+
+const (
+	WallTile TileType = iota
+	FloorTile
+	EnemyTile
+	DungeonEntrance
+	DungeonExit
+	WaterTile
+	WeaponTile
+	ChestTile
+	FenceTile
+	CampfireTile
+	BoatTile
+	DockTile
+	TreeTile
+	TreeBaseTile
+)
+
+type Map struct {
+	Width, Height  int
+	Tiles          [][]TileType
+	WallTextures   [][]string
+	FloorTextures  [][]string
+	FloorRotations [][]float64
+	Start          Position
+	Exit           Position
+	Respawn        Position
+	DungeonEntry   Position
+}
