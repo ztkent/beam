651a7d9adfc6afdc9c5272c59e9908ceb6b77df0
ztkent <ztkent@gmail.com>
2025-04-29 23:59:18 -0400

    fix issue rendering after deleting part of a complex texture

--- textures.go
+++ textures.go
@@ -35,8 +35,10 @@ func (t *AnimatedTexture) GetCurrentFrame(currentTime float64) Texture {
 			if t.CurrentFrame >= len(t.Frames) {
 				t.CurrentFrame = 0
 			}
+			return t.Frames[t.CurrentFrame]
+		} else {
+			return t.Frames[0]
 		}
-		return t.Frames[t.CurrentFrame]
 	}
 	return t.Frames[0]
 }
